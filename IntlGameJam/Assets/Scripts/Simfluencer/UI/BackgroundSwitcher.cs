1a3b65f076a0d2c1a3d04033a5b7c6056f109372
Casper van Battum <cvanbattum@gmail.com>
2020-02-07 11:35:22 +0100

    Fix Merge

---
+++ IntlGameJam/Assets/Scripts/Simfluencer/UI/BackgroundSwitcher.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+namespace Simfluencer.UI {
+    [RequireComponent(typeof(SpriteRenderer))]
+    public class BackgroundSwitcher : MonoBehaviour {
+        [SerializeField] private Sprite neutralBackground;
+        [SerializeField] private Sprite badBackground;
+        [SerializeField] private Sprite goodBackground;
+
+        private new SpriteRenderer renderer;
+
+        private void Awake() {
+            renderer = GetComponent<SpriteRenderer>();
+            renderer.sprite = neutralBackground;
+        }
+        
+        
+    }
+}
