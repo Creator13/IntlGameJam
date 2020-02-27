fedfb1b8da8eb03db2074e58089606a28fe91860
Casper van Battum <cvanbattum@gmail.com>
2020-02-27 15:50:05 +0100

    Program button click sound effect to happen on mouse down instead of mous click

--- Simfluencer/Assets/Scripts/cvanbattum/Audio/ButtonSoundEffect.cs
+++ Simfluencer/Assets/Scripts/cvanbattum/Audio/ButtonSoundEffect.cs
@@ -1,15 +1,23 @@
-﻿using UnityEngine;
+﻿using Simfluencer;
+using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 namespace cvanbattum.Audio {
-    [RequireComponent(typeof(Button))]
+    [RequireComponent(typeof(Button), typeof(EventTrigger))]
     public class ButtonSoundEffect : SoundEffectPlayer {
         private Button button;
         
         private void Awake() {
             button = GetComponent<Button>();
-            
-            button.onClick.AddListener(PlaySound);
+            var evtTrig = GetComponent<EventTrigger>();
+            var entry = new EventTrigger.Entry();
+            entry.eventID = EventTriggerType.PointerDown;
+            entry.callback.AddListener((data) => {
+                // var ptrData = data as PointerEventData;
+                SoundManager.Instance.PlayEffect(soundName);
+            });
+            evtTrig.triggers.Add(entry);
         }
     }
 }
