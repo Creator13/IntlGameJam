97e0668f0016a2f42c88527dbab38bb5053e8ccc
Casper van Battum <cvanbattum@gmail.com>
2020-03-12 12:57:22 +0100

    Added icon selection

---
+++ Simfluencer/Assets/Scripts/Simfluencer/UI/ProfilePictureChooser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Simfluencer.UI {
+    [RequireComponent(typeof(ToggleGroup))]
+    public class ProfilePictureChooser : MonoBehaviour {
+        private ToggleGroup toggleGroup;
+        
+        private void Awake() {
+            toggleGroup = GetComponent<ToggleGroup>();
+        }
+
+        public Sprite GetSelected() {
+            var toggle = toggleGroup.ActiveToggles().First();
+            var profilePicture = toggle.GetComponent<ProfileToggle>();
+            return profilePicture.Sprite;
+        }
+    }
+}
---
+++ Simfluencer/Assets/Scripts/Simfluencer/UI/Screens/ProfileScreen.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Simfluencer.UI.Screens {
+    public class ProfileScreen : Screen {
+        [SerializeField] private string nextScreen;
+
+        protected override void Show() {
+            //TODO check for existing profile
+            
+            
+        }
+    }
+}
